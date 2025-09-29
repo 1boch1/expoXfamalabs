fac26016b49ac9cc837efdd47b6f2387577e0471
Mirco Bochicchio <mircobochicchio1@gmail.com>
2025-09-29 16:24:51 +0200

    Test di navigazione Stack e Router
    Navigazione sfruttando <Stack/> di default del Layout della root e useRouter() hook

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, View } from "react-native";
 
-export default function Index() {
+export default function Index()
+{
+  const router = useRouter();
+
   return (
     <View
       style={{
@@ -9,7 +13,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Vai alla compilazione" onPress={() => router.navigate('/compilaSurvey')} />
+      <Button title="Vai alla lista" onPress={() => router.navigate('/listaSurvey')} />
     </View>
   );
 }
